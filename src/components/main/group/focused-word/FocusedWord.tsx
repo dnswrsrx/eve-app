7f2b7ac0eb49e02f1befe42b8f1e72cbcb8ec811
dinosawrusrex <dennissaw12@gmail.com>
2021-08-12 23:08:23 -0700

    Handle no definitions and phonetics when word is selected on client side

--- src/components/main/group/focused-word/FocusedWord.tsx
+++ src/components/main/group/focused-word/FocusedWord.tsx
@@ -14,8 +14,8 @@ const FocusedWord = ({ word, wordInfo }: FocusedWordProps) => {
   const formattedWord = word.replace(' ', '_');
   const definitions = wordInfo[word].apiDefinitions;
 
-  const phonetics = definitions.map((d: Definitions) => d.phonetics.map(p => p.audio)).flat();
-  const apiDefinitions = definitions.map((d: Definitions) => d.definitions.filter(d => d.selected)).flat();
+  const phonetics = definitions?.map((d: Definitions) => d.phonetics.map(p => p.audio)).flat();
+  const apiDefinitions = definitions?.map((d: Definitions) => d.definitions.filter(d => d.selected)).flat();
 
   return (
     <div className="focused-word">
@@ -39,7 +39,7 @@ const FocusedWord = ({ word, wordInfo }: FocusedWordProps) => {
           </div>
         }
 
-        { phonetics.length ?
+        { phonetics?.length ?
           <>
             <h4 className="focused-word__heading-word">Pronunciations</h4>
             {
@@ -53,48 +53,43 @@ const FocusedWord = ({ word, wordInfo }: FocusedWordProps) => {
           : <></>
         }
 
-        { wordInfo[word].customDefinition || apiDefinitions.length ?
+        <h4 className="focused-word__heading-word">Definitions</h4>
+        { wordInfo[word].customDefinition || apiDefinitions?.length ?
+          <div className="focused-word__definition-list">
+            { wordInfo[word].customDefinition &&
+                <div className="focused-word__definition-wrapper" dangerouslySetInnerHTML={{ __html: wordInfo[word].customDefinition }}></div>
+            }
 
-          <>
-            <h4 className="focused-word__heading-word">Definitions</h4>
-
-            <div className="focused-word__definition-list">
-
-              { wordInfo[word].customDefinition &&
-                  <div className="focused-word__definition-wrapper" dangerouslySetInnerHTML={{ __html: wordInfo[word].customDefinition }}></div>
-              }
-
-              { apiDefinitions.length ?
-                <>
-                  { apiDefinitions.map((definition: Definition, index: number) => {
-                    return <div key={`${definition.definition}-${index}`} className='focused-word__definition-wrapper'>
-                      <p className="focused-word__type"><span className="bold">Type:</span> { definition.type }</p>
-                      <p className="focused-word__definition"><span className="bold">Definition:</span> { definition.definition }</p>
-                      {
-                        definition.example &&
-                        <p className="focused-word__example"><span className="bold">Example:</span> { definition.example }</p>
-                      }
-                      {
-                        definition.synonyms &&
-                        <>
-                          <p><span className="bold">Synonyms:</span> </p>
-                          <div className="focused-word__synonyms">
-                            { definition.synonyms.map((synonym: string) => (
-                              <span key={synonym} className="focused-word__synonym">
-                                { synonym }
-                              </span>
-                            ))}
-                          </div>
-                        </>
-                      }
-                    </div>
-                  })}
-                </>
-                : <></>
-              }
-
-            </div>
-          </> : <></>
+            { apiDefinitions?.length ?
+              <>
+                { apiDefinitions.map((definition: Definition, index: number) => {
+                  return <div key={`${definition.definition}-${index}`} className='focused-word__definition-wrapper'>
+                    <p className="focused-word__type"><span className="bold">Type:</span> { definition.type }</p>
+                    <p className="focused-word__definition"><span className="bold">Definition:</span> { definition.definition }</p>
+                    {
+                      definition.example &&
+                      <p className="focused-word__example"><span className="bold">Example:</span> { definition.example }</p>
+                    }
+                    {
+                      definition.synonyms &&
+                      <>
+                        <p><span className="bold">Synonyms:</span> </p>
+                        <div className="focused-word__synonyms">
+                          { definition.synonyms.map((synonym: string) => (
+                            <span key={synonym} className="focused-word__synonym">
+                              { synonym }
+                            </span>
+                          ))}
+                        </div>
+                      </>
+                    }
+                  </div>
+                })}
+              </>
+              : <></>
+            }
+          </div>
+          : <p>Definitions were not provided for this term. Please click on the Merriam Webster dictionary link instead.</p>
         }
       </div>
     </div>
