9797fe319a1ae11d09e27b90a0da033c8b21ccaf
dinosawrusrex <dennissaw12@gmail.com>
2021-08-14 11:18:23 -0700

    Remove unused imports
    Thanks TS

--- src/components/main/group/focused-word/FocusedWord.tsx
+++ src/components/main/group/focused-word/FocusedWord.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { WordList, Definitions, Phonetic, Definition } from '../../../models/models';
+import { WordList, Definitions, Definition } from '../../../models/models';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import './FocusedWord.scss';
