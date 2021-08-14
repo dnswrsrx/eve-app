3818e831b3ae2968e576c2f35a09eee9da97f6fd
dinosawrusrex <dennissaw12@gmail.com>
2021-08-14 15:52:45 -0700

    Custom hook returning current user's subscription or if it's same as what we provide
    The hook takes an optional wordCategory that we then use to check
    against the user's subscription.
    
    Will be where we need to get user's subscription or check it against a
    known one.

---
+++ src/utils/userSubscription.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import firebase from '../config/firebaseConfig';
+
+const useSubscription = (wordCategory: string|null = null): string|boolean => {
+  const [subscription, setSubscription] = useState('');
+  const user = firebase.auth().currentUser;
+
+  useEffect(() => {
+    if (user) {
+      firebase.firestore().collection('users').doc(user.uid)
+        .onSnapshot(observer => setSubscription(observer?.data()?.main))
+    }
+  }, [user])
+
+  if (wordCategory) return subscription.includes(wordCategory);
+
+  return subscription;
+}
+
+export default useSubscription;
