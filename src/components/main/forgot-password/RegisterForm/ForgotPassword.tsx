20966d4eda439b12471557bf09c2ed2ef515493d
dinosawrusrex <dennissaw12@gmail.com>
2021-08-31 01:02:45 -0700

    Add success message when password reset link sent, and add the 500px width

--- src/components/main/forgot-password/RegisterForm/ForgotPassword.tsx
+++ src/components/main/forgot-password/RegisterForm/ForgotPassword.tsx
@@ -4,17 +4,17 @@ import firebase from '../../../../config/firebaseConfig';
 import './ForgotPassword.scss';
 
 const RegisterForm = (): JSX.Element => {
-  const [authError, setAuthError] = useState('');
+  const [resetResponse, setResetResponse] = useState<string>('');
   const [submitting, setSubmitting] = useState(false);
   const { register, handleSubmit, errors } = useForm();
 
   const onSubmit = (data: any) : void => {
     setSubmitting(true);
-    firebase.auth().sendPasswordResetEmail(data.email).then((response: any) => {
-      console.log(response);
+    firebase.auth().sendPasswordResetEmail(data.email).then(() => {
+      setResetResponse('success');
       setSubmitting(false);
     }).catch((error: {message: string}) => {
-      setAuthError(error.message);
+      setResetResponse(error.message);
       setSubmitting(false);
     });
   }
@@ -33,7 +33,11 @@ const RegisterForm = (): JSX.Element => {
         />
         { errors.email && <p className="register-form__error error">{ errors.email.message }</p> }
       </div>
-      { authError && <p className="register-form__error error">{ authError }</p> }
+      { resetResponse && 
+        resetResponse === 'success'
+          ? <p className="register-form_success success">A password reset link has been sent. Please check your email.</p>
+          : <p className="register-form__error error">{ resetResponse }</p>
+      }
       <div className="register-form__row">
         { submitting && <span className="register-form__spinner" aria-hidden="true"></span> }
         <button className="register-form__submit" type="submit" disabled={submitting}>Request Link</button>
