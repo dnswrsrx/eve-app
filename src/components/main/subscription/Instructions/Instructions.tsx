8344b5c69a25155229734e59ed30d20973ac852f
dinosawrusrex <dennissaw12@gmail.com>
2022-05-15 14:41:01 -0700

    Add instructions section to subscription page
    Instruction section replaces the how it works section and move how it
    works section below.
    
    Also detect if there's "instructions" in the URL as a hash, so taht we
    can emphasise the instructions section. Thinking of having a link to
    this page from the home page with the instructions to be emphasised.
    
    Drafted some instructions based on suggestions from Gerry.

---
+++ src/components/main/subscription/Instructions/Instructions.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { FirebaseReducer } from 'react-redux-firebase';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+import './Instructions.scss';
+
+interface InstructionsProps {
+  auth: FirebaseReducer.AuthState,
+  subscription: string|boolean|null
+}
+
+const Instructions = ({ auth, subscription }: InstructionsProps): JSX.Element => {
+
+  const emphasised = window.location.hash.includes('instructions');
+
+  return (
+    <article className={`instructions ${emphasised ? 'emphasised' : ''}`}>
+      <h2>
+        Instructions
+        {emphasised && <span><FontAwesomeIcon icon={faStar} /></span>}
+      </h2>
+
+      <details open={!auth.uid}>
+        <summary>
+          Sign Up
+        </summary>
+        <ol>
+          <li>
+            To subscribe, you first need to sign up.
+            Complete the Sign Up form.
+          </li>
+          <li>
+            After clicking Sign Up, you will receive an email to verify your email address.
+          </li>
+        </ol>
+      </details>
+
+      <details open={Boolean(auth.uid && !auth.emailVerified)}>
+        <summary>
+          Verify Email Address
+        </summary>
+        <ol>
+          <li>
+            Go to your email and click the message with the subject: <strong>Verify your email for English Vocabulary Exercises</strong>.
+          </li>
+          <li>
+            You will see the message: <strong>Hello. Follow this link to verify your email address. </strong>
+            Click on the link.
+          </li>
+          <li>
+            A new tab or window will open saying that your email has been verified. You may close that tab or window.
+          </li>
+          <li>
+            Refresh this page and you can start subscribing!
+          </li>
+        </ol>
+      </details>
+
+      <details open={Boolean(auth.emailVerified && !subscription)}>
+        <summary>
+          Subscribe
+        </summary>
+        <ol>
+          <li>You can now choose the tier you would like to subscribe above.</li>
+          <li>When you've clicked on a <strong>Subscribe</strong> button, you will be brought to a new page to enter your credit card information.</li>
+          <li>Once you're subscribed, you can access all the groups and exercises in the tier you chose.</li>
+        </ol>
+      </details>
+    </article>
+  )
+}
+
+export default Instructions;
--- src/components/main/subscription/Subscription.tsx
+++ src/components/main/subscription/Subscription.tsx
@@ -3,6 +3,7 @@ import { useSelector, shallowEqual } from 'react-redux';
 import { RootState } from '../../../store/reducers/rootReducer';
 
 import Loading from '../../general/loading/Loading';
+import Instructions from './Instructions/Instructions';
 import RegisterForm from './RegisterForm/RegisterForm';
 
 import useSubscription from '../../../utils/userSubscription';
@@ -32,36 +33,40 @@ const Subscription = (): JSX.Element => {
         <hr />
 
         <div className={!auth.uid ? "subscription-page__register" : ""}>
-          <div>
-            <h2>How It Works</h2>
-            <ul>
-              <li>
-                We use Stripe Billing to manage subscriptions and process payments.
-                You may subscribe once you sign up and verify the email address provided.
-              </li>
-              <li>
-                Stripe will securely store your credit card details that you provide for the subscription. You may update these details on the customer portal.
-              </li>
-              <li>
-                Every year while you are subscribed, Stripe will charge the credit card on your account.
-                You will be notified 7 days before you are charged.
-              </li>
-              <li>
-                You may change your subscription on the customer portal and it should be reflected immediately.
-                The price difference when changing tiers will be prorated to the remaining time left for that year.
-                <strong> Note:</strong> if you switch to a tier with a lower rate, the prorated difference will be stored on your Stripe customer account. This difference will then be applied on your next payment.
-              </li>
-              <li>
-                You may cancel your subscription on the customer portal.
-                This will inform Stripe to not renew your subscription when the next payment is due.
-              </li>
-              <li>
-                Your billing history is available for you to view on the customer portal.
-              </li>
-            </ul>
-          </div>
+          <Instructions auth={auth} subscription={subscription} />
           {!auth.uid && <RegisterForm /> }
         </div>
+
+        <hr />
+
+        <article>
+          <h2>How It Works</h2>
+          <ul>
+            <li>
+              We use Stripe Billing to manage subscriptions and process payments.
+              You may subscribe once you sign up and verify the email address provided.
+            </li>
+            <li>
+              Stripe will securely store your credit card details that you provide for the subscription. You may update these details on the customer portal.
+            </li>
+            <li>
+              Every year while you are subscribed, Stripe will charge the credit card on your account.
+              You will be notified 7 days before you are charged.
+            </li>
+            <li>
+              You may change your subscription on the customer portal and it should be reflected immediately.
+              The price difference when changing tiers will be prorated to the remaining time left for that year.
+              <strong> Note:</strong> if you switch to a tier with a lower rate, the prorated difference will be stored on your Stripe customer account. This difference will then be applied on your next payment.
+            </li>
+            <li>
+              You may cancel your subscription on the customer portal.
+              This will inform Stripe to not renew your subscription when the next payment is due.
+            </li>
+            <li>
+              Your billing history is available for you to view on the customer portal.
+            </li>
+          </ul>
+        </article>
       </div>
     </section>
   )
