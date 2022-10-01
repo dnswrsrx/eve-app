f3576e085f74e5f220582e64976cb2898ad49d39
dinosawrusrex <dennissaw12@gmail.com>
2022-10-01 01:05:39 -0700

    Pull cart and portal loading into its own hook
    Trying to separate the institutional subscription section a bit more
    from the existing individual category subscription. Since these
    subscription units require the same cart and portal functionality, but
    they might not reside closely to each other, make sense to pull the cart
    and portal so that it can be shared between different subscription
    components.

---
+++ src/components/main/utils/subscribe/CartOrPortalHook.tsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import { isEqual } from 'lodash';
+import { useSelector } from 'react-redux';
+
+import firebase from '../../../../config/firebaseConfig';
+import stripePromise from '../../../../config/stripeConfig';
+
+import { RootState } from '../../../../store/reducers/rootReducer';
+
+import useSubscription from '../../../../utils/userSubscription';
+
+
+interface hookSignature {
+  cartOrPortal: Function,
+  loadingCartOrPortal: string|null,
+  error: string|null
+}
+
+
+const useCartOrPortalHook = (): hookSignature => {
+
+  const auth = useSelector((state: RootState) => state.firebase.auth, isEqual);
+  const user = auth.isLoaded && !auth.isEmpty && firebase.firestore().collection('users').doc(auth.uid);
+
+  const [error, setError] = useState<'cart'|'portal'|null>(null);
+  const [loadingCartOrPortal, setLoadingCartOrPortal] = useState<'cart'|'portal'|null>(null);
+
+  const subscription = useSubscription();
+
+  const customerPortal = () => {
+    const getPortalLink = firebase.app().functions('us-central1').httpsCallable('ext-firestore-stripe-subscriptions-createPortalLink');
+
+    setLoadingCartOrPortal('portal');
+
+    getPortalLink({ returnUrl: window.location.origin }).then(({data}) => {
+      window.location.assign(data.url);
+    }).catch(e => {
+      console.log(e);
+      setError('portal');
+      setLoadingCartOrPortal(null);
+    })
+  }
+
+  const checkout = (priceID: string) => {
+
+    if (user) {
+      setLoadingCartOrPortal('cart');
+
+      // Set up a checkout session that is inserted into Firestore.
+      // Once session is in Firestore, it'll ping Stripe to verify.
+      user.collection('checkout_sessions').add({
+        price: priceID,
+        customerEmail: auth.email,
+        mode: 'subscription',
+        success_url: window.location.href,
+        cancel_url: window.location.href,
+      }).then((snap: firebase.firestore.DocumentData) => {
+
+        snap.onSnapshot((snapshot: firebase.firestore.DocumentSnapshot) => {
+
+
+          // If verification successful, Stripe will insert a sessionId into this checkout_session document.
+          const sessionId: string = snapshot?.data()?.sessionId;
+
+          // Using that sessionId, we redirect to Stripe's checkout page.
+          // User can supply the card details for the subscription.
+          if (sessionId) {
+            stripePromise.then(stripe => {
+              stripe?.redirectToCheckout({sessionId})
+            }).catch(e => {
+              setError('cart');
+              setLoadingCartOrPortal(null);
+            })
+          }
+        }, (e: string) => {
+          setError('cart');
+        })
+      }).catch(e => {
+        setError('cart');
+        setLoadingCartOrPortal(null);
+      })
+    }
+  }
+
+  const cartOrPortal = (productName: string): Function => {
+    if (user && auth.uid && auth.emailVerified) {
+      if (!subscription) return checkout;
+      if (subscription !== productName) return customerPortal;
+    }
+    return () => {};
+  }
+
+  return { cartOrPortal, loadingCartOrPortal, error }
+}
+
+
+export default useCartOrPortalHook;
--- src/components/main/utils/subscribe/Subscribe.tsx
+++ src/components/main/utils/subscribe/Subscribe.tsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { isEqual } from 'lodash';
 import { useSelector } from 'react-redux';
 import { useFirestoreConnect, isLoaded } from 'react-redux-firebase';
-
-import firebase from '../../../../config/firebaseConfig';
-import stripePromise from '../../../../config/stripeConfig';
 
 import { RootState } from '../../../../store/reducers/rootReducer';
 import { CollectionNames, Product } from '../../../models/models';
@@ -13,7 +10,8 @@ import Loading from '../../../general/loading/Loading';
 
 import './Subscribe.scss';
 import SubscribeToProduct from './SubscribeToProduct';
-import useSubscription from '../../../../utils/userSubscription';
+
+import useCartOrPortal from '../../utils/subscribe/CartOrPortalHook';
 
 const Subscribe = (): JSX.Element => {
 
@@ -22,77 +20,10 @@ const Subscribe = (): JSX.Element => {
 
   const auth = useSelector((state: RootState) => state.firebase.auth, isEqual);
 
-  const user = auth.isLoaded && !auth.isEmpty && firebase.firestore().collection('users').doc(auth.uid);
-
-  const [error, setError] = useState<'cart'|'portal'|null>(null);
-  const [loadingCartPortal, setLoadingCartPortal] = useState<'cart'|'portal'|null>(null);
-
-  const subscription = useSubscription();
+  const { cartOrPortal, loadingCartOrPortal, error } = useCartOrPortal();
 
   if (!isLoaded(products) || !auth.isLoaded) return <Loading />;
 
-  const _customerPortal = () => {
-    const getPortalLink = firebase.app().functions('us-central1').httpsCallable('ext-firestore-stripe-subscriptions-createPortalLink');
-
-    setLoadingCartPortal('portal');
-
-    getPortalLink({ returnUrl: window.location.origin }).then(({data}) => {
-      window.location.assign(data.url);
-    }).catch(e => {
-      console.log(e);
-      setError('portal');
-      setLoadingCartPortal(null);
-    })
-  }
-
-  const _checkout = (priceID: string) => {
-
-    if (user) {
-      setLoadingCartPortal('cart');
-
-      // Set up a checkout session that is inserted into Firestore.
-      // Once session is in Firestore, it'll ping Stripe to verify.
-      user.collection('checkout_sessions').add({
-        price: priceID,
-        customerEmail: auth.email,
-        mode: 'subscription',
-        success_url: window.location.href,
-        cancel_url: window.location.href,
-      }).then((snap: firebase.firestore.DocumentData) => {
-
-        snap.onSnapshot((snapshot: firebase.firestore.DocumentSnapshot) => {
-
-
-          // If verification successful, Stripe will insert a sessionId into this checkout_session document.
-          const sessionId: string = snapshot?.data()?.sessionId;
-
-          // Using that sessionId, we redirect to Stripe's checkout page.
-          // User can supply the card details for the subscription.
-          if (sessionId) {
-            stripePromise.then(stripe => {
-              stripe?.redirectToCheckout({sessionId})
-            }).catch(e => {
-              setError('cart');
-              setLoadingCartPortal(null);
-            })
-          }
-        }, (e: string) => {
-          setError('cart');
-        })
-      }).catch(e => {
-        setError('cart');
-        setLoadingCartPortal(null);
-      })
-    }
-  }
-
-  const cartOrPortal = (productName: string): Function => {
-    if (user && auth.uid && auth.emailVerified) {
-      if (!subscription) return _checkout;
-      if (subscription !== productName) return _customerPortal;
-    }
-    return () => {};
-  }
 
   return (
     <div className="subscribe">
@@ -105,7 +36,7 @@ const Subscribe = (): JSX.Element => {
             key={index}
             product={p}
             cartOrPortal={cartOrPortal(p.name)}
-            loadingCartPortal={loadingCartPortal}
+            loadingCartPortal={loadingCartOrPortal}
             auth={auth}
           />
         })}
