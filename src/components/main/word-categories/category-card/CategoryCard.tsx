8950da03c41c3ee266a42df006dd29629f1c61fa
dinosawrusrex <dennissaw12@gmail.com>
2023-05-12 16:57:34 -0700

    Add images in word categories card

--- src/components/main/word-categories/category-card/CategoryCard.tsx
+++ src/components/main/word-categories/category-card/CategoryCard.tsx
@@ -8,11 +8,15 @@ interface CategoryCardProps {
 }
 
 const CategoryCard = ({ category }: CategoryCardProps): JSX.Element => {
+  const imageUrl = `/images/${category.name.includes('General') ? 'friends' : 'educator'}.svg`
+  const altText = `/images/${category.name.includes('General') ? 'coffee with friends' : 'classroom'}.svg`
+
   return (
     <Link to={`/subcategories/${category.id}`} className="category-card">
       <h2 className="category-card__heading">
         {category.name}
       </h2>
+      <img src={ imageUrl } alt={ altText } />
     </Link>
   )
 }
