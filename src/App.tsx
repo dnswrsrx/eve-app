fc65dd3a21db6a7ce82a778052ea588640729e2d
dinosawrusrex <dennissaw12@gmail.com>
2023-09-02 21:26:27 -0700

    Deprecate Login via admin area - only can login via the regular way

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './components/main/Main';
 import Admin from './components/admin/Admin';
-import AdminLogin from './components/admin/login/Login';
 import './App.scss';
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/admin-login" component={AdminLogin} />
         <Route path="/admin-dashboard" component={Admin} />
         <Route path="/" component={Main} />
       </Routes>
