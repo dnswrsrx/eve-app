7050a6c18f0dfb25c474fddb69d43f679e5890c2
dnswrsrx <xtnct@dnswrsrx.com>
2024-07-22 00:59:05 -0700

    Add observer handling to remove google's injected styling
    within Ads component

--- src/components/main/ads/Ads.tsx
+++ src/components/main/ads/Ads.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Adsense } from '@ctrl/react-adsense';
 import { AuthContext } from '../Main';
 import useSubscription from '../utils/UserSubscriptionHook';
@@ -13,6 +13,29 @@ const Ads = ({ slot }: AdsProp): JSX.Element => {
 
   const auth = useContext(AuthContext);
   const isSubscribed = useSubscription();
+
+  useEffect(() => {
+    const elements = [document.querySelector('div#root') as HTMLElement, document.querySelector('div#root > main') as HTMLElement]
+
+    const observer = new MutationObserver((mutations, observer) => {
+      mutations.forEach(m => {
+        const elem = m.target as HTMLElement
+        elem.style.height = "";
+        elem.style.minHeight = "";
+      })
+    })
+
+    elements.forEach(e => observer.observe(e, {
+      attributes: true,
+      attributeFilter: ['style']
+    }))
+
+    setTimeout(() => {
+      elements.forEach((e: HTMLElement) => e.style.height = "100%")
+    }, 1000)
+
+    return () => observer.disconnect();
+  }, [])
 
   if (!pathsToHideAds.includes(window.location.pathname)) {
     if (!auth.uid || !isSubscribed) {
