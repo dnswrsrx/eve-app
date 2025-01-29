b46eb75e1df464389fcccf03149ddf394e7793b9
dnswrsrx <xtnct@dnswrsrx.com>
2025-01-29 00:36:56 -0800

    Redirect some known legacy routes to current route

--- src/components/general/404/PageNotFound.tsx
+++ src/components/general/404/PageNotFound.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './PageNotFound.scss';
 
 const PageNotFound = (): JSX.Element => {
+  const navigate = useNavigate()
+
+  const p = window.location.pathname
+
+  if (p === '/index.html' || p === '/index.htm' || p === '/index') {
+    navigate('/')
+  } else if (p.includes('/eve-exercises') || p.includes('/general-vocabulary')) {
+    navigate('/subcategories/0zwuF8q0eHklNDFZJAH9')
+  } else if (p.includes('/academic-word-list') || p.includes('/AWL')) {
+    navigate('/subcategories/VQvdu2OoscCzjxPk1C5s')
+  }
+
   return (
     <section className="page-not-found">
       <div className="page-not-found__wrapper page-wrapper">
