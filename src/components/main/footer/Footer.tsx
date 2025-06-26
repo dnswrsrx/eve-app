a9bb33b2cc5cb51ec513e771846c597dbae0426a
dnswrsrx <xtnct@dnswrsrx.com>
2025-06-26 10:26:53 -0700

    Also remove notes to teacher in footer

--- src/components/main/footer/Footer.tsx
+++ src/components/main/footer/Footer.tsx
@@ -7,7 +7,6 @@ const Footer = (): JSX.Element => {
       <div className="main-footer__wrapper page-wrapper">
         <div className="main-footer__link-container">
           <a rel="noopener noreferrer" className="main-footer__link" href="/terms-of-use">Terms of Use</a>
-          <a rel="noopener noreferrer" className="main-footer__link" href="/teacher-notes">Note to Teachers</a>
           <a rel="noopener noreferrer" target="_blank" className="main-footer__link" href="https://www.facebook.com/pages/English-Vocabulary-Exercises/179726835407325">Facebook</a>
         </div>
         <div className="main-footer__link-container">
